809ea03d95f0314876ed4e3463d43703f974e705
kamrul2006 <kamrulislamapurba@gmail.com>
2025-04-11 22:22:15 +0600

    Banner  added

--- src/Home/Banner.jsx
+++ src/Home/Banner.jsx
@@ -7,11 +7,13 @@ export default function Banner() {
                 <h1 className="text-4xl md:text-5xl font-extrabold mb-6">
                     Stand with <span className="text-red-500">Palestine</span>, Voice of <span className="text-green-500">Bangladesh</span>
                 </h1>
+
+                
                 <p className="text-lg text-gray-300 mb-8 max-w-2xl mx-auto">
                     Join us in supporting justice, peace, and human rights. Your voice matters. Together, we can make a difference.
                 </p>
                 
-                
+
                 <Link
                     to="/support"
                     className="inline-block bg-red-600 hover:bg-red-700 text-white font-semibold px-8 py-3 rounded-2xl transition"
